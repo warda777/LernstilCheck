b758b0cf3b2cac1c7c3b74c98fcb06becf41e45b
Andreas Warda <andreas.warda@gmail.com>
2025-09-04 08:31:25 +0200

    Initial Commit

---
+++ LernstilCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LernstilCheck
+//
+//  Created by Andreas Warda on 04.09.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
